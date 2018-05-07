60589a698218594905ea018677a0cbc2d33e7077
aztest01 <aztest01@outlook.com>
2018-05-07 18:55:45 +0000

    Passing test

--- testapp.py
+++ testapp.py
@@ -2,7 +2,7 @@ import unittest
 
 class Test_test1(unittest.TestCase):
     def test_A(self):
-        self.fail("Not implemented")
+        self.assertEqual(1,1)
 
 if __name__ == '__main__':
     unittest.main()
